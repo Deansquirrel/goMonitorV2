fe11271a3a673ae3be3f9a5385604c15cb5dd054
yuanxs <yuanxs@95szzl.cn>
2019-03-14 17:04:23 +0800

    0.0.2

--- global/Global.go
+++ global/Global.go
@@ -10,7 +10,7 @@ import (
 const (
 	//PreVersion = "0.0.1 Build20190313"
 	//TestVersion = "0.0.0 Build20190101"
-	Version = "0.0.0 Build20190101"
+	Version = "0.0.2 Build20190314"
 )
 
 var SysConfig *config.SysConfig
